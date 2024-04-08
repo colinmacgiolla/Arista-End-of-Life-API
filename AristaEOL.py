05bd5e6ca4ade46f530f1e8c424a9fd0eab1c03e
Colin MacGiollaEain <colin@arista.com>
2024-04-08 16:48:31 +0100

    Initial commit

---
+++ AristaEOL.py
@@ -0,0 +1,128 @@
+#!/bin/python
+
+import base64
+import sys
+from urllib.error import HTTPError
+import logging
+import json
+import requests
+
+log = logging.getLogger(__name__)
+
+
+class AristaEOL():
+    """A class to interact with Arista End-of-Life (EOL) API.
+
+    This class provides methods to authenticate with the Arista EOL API and
+    check hardware or software End-of-Life status.
+    """
+    def __init__(self, token) -> None:
+        self.user_token = token
+        self._encode_token()
+        self._authenticate()
+
+    def _encode_token(self) -> None:
+        """
+        Encodes the user token using base64.
+
+        Sets the encoded_token attribute with the base64 encoded version
+        of the user token.
+        """
+        self.encoded_token = base64.encodebytes(self.user_token.encode('ascii'))
+        log.debug("Encoded token: %s", self.encoded_token)  # Use debug for token details
+
+    def _authenticate(self):
+        """
+        Authenticates with the Arista EOL API.
+
+        Establishes a session with the Arista EOL API using the provided token.
+        Raises an exception on authentication failure.
+
+        Sets the cookie attribute with the session code obtained from the response.
+        """
+        url = 'https://www.arista.com/api/sessionCode/'
+        headers = {}
+        headers['content-type'] = 'application/json'
+        data = {}
+        data['accessToken'] = self.encoded_token.decode("utf-8")
+        try:
+            r = requests.post(url, json=data, headers=headers)
+            r.raise_for_status()
+        except HTTPError as http_err:
+            log.error("HTTP Error occurred: %s", http_err)
+            sys.exit(1)
+        except Exception as err:
+            log.error("Error getting session token: %s", err)
+            sys.exit(1)
+
+        log.info("Successfully authenticated")
+        raw_resp = r.text
+        resp = json.loads(raw_resp)
+        self.cookie = resp['data']['session_code']
+
+    def hardware_check(self, sku: str) -> dict:
+        """
+        Checks the End-of-Life status of a hardware SKU.
+
+        Queries the Arista EOL API for the End-of-Life details of the provided hardware SKU.
+        This needs to be the full SKU e.g. DCS-7150S-52-CL-R
+
+        Args:
+            sku (str): The hardware SKU to check.
+
+        Returns:
+            dict: A dictionary containing the hardware EOL information.
+        """
+        url = 'https://www.arista.com/api/eox/hwLifecycle/'
+        headers = {}
+        headers['content-type'] = 'application/json'
+        data = {}
+        data['sessionCode'] = self.cookie
+        data['mainSku'] = sku
+
+        try:
+            r = requests.post(url, json=data, headers=headers)
+            r.raise_for_status()
+        except HTTPError as http_err:
+            log.error("HTTP Error occurred: %s", http_err)
+            sys.exit(1)
+        except Exception as err:
+            log.error("Error downloading Alertbase: %s", err)
+            sys.exit(1)
+
+        raw_resp = r.text
+        resp = json.loads(raw_resp)
+
+        return resp['data']
+
+    def software_check(self, releaseTrain: str) -> None:
+        """
+        Checks the End-of-Life status of a software release train.
+
+        Args:
+            releaseTrain (str): The software release train to check e.g. 4.28
+
+        Returns:
+            dict: A dictionary containing the software EOL information.
+        """
+        url = 'https://www.arista.com/api/eox/swLifecycle/'
+        headers = {}
+        headers['content-type'] = 'application/json'
+        data = {}
+        data['sessionCode'] = self.cookie
+        data['releaseTrain'] = releaseTrain
+
+        try:
+            r = requests.post(url, json=data, headers=headers)
+            r.raise_for_status()
+        except HTTPError as http_err:
+            log.error("HTTP Error occurred: %s", http_err)
+            sys.exit(1)
+        except Exception as err:
+            log.error("Error downloading Alertbase: %s", err)
+            sys.exit(1)
+
+        raw_resp = r.text
+        resp = json.loads(raw_resp)
+
+        return resp['data']
---
+++ Readme.md
@@ -0,0 +1,68 @@
+# AristaEOL Python Class
+
+This Python class provides a way to interact with the Arista End-of-Life (EOL) API. It allows you to:
+* Authenticate with the Arista EOL API using your access token.
+* Check the End-of-Life (EOL) status of Arista hardware by SKU.
+* Check the End-of-Life (EOL) status of Arista software release trains.
+
+## Installation
+
+You can install the required libraries using the pip command:
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Here's a basic example of how to use the AristaEOL class:
+```python
+
+from AristaEOL import AristaEOL
+
+# Replace with your Arista API access token
+access_token = "YOUR_ACCESS_TOKEN"
+
+# Create an AristaEOL object
+arista_eol = AristaEOL(access_token)
+
+# Check hardware EOL status for a specific SKU
+sku = "DCS-7150S-52-CL-F"
+hardware_eol_info = arista_eol.hardware_check(sku)
+print(hardware_eol_info)
+
+# Check software EOL status for a release train
+release_train = "4.23"
+software_eol_info = arista_eol.software_check(release_train)
+print(software_eol_info)
+```
+
+Note: Replace YOUR_ACCESS_TOKEN with your actual Arista API access token.
+
+## API Documentation
+
+Here's a detailed explanation of the class methods:
+
+    __init__(self, token: str) -> None
+        Initializes the AristaEOL object.
+        Takes the Arista API access token as a string argument.
+        Internally calls _encode_token and _authenticate methods.
+
+    _encode_token(self) -> None
+        Encodes the user token using base64.
+        Sets the encoded_token attribute with the base64 encoded version of the user token.
+
+    _authenticate(self) -> None
+        Authenticates with the Arista EOL API.
+        Establishes a session with the API using the provided token.
+        Raises an exception on authentication failure.
+        Sets the cookie attribute with the session code obtained from the response.
+
+    hardware_check(self, sku: str) -> dict
+        Checks the End-of-Life status of a hardware SKU.
+        Takes the hardware SKU as a string argument.
+        Returns a dictionary containing the hardware EOL information.
+
+    software_check(self, releaseTrain: str) -> dict
+        Checks the End-of-Life status of a software release train.
+        Takes the software release train as a string argument.
+        Returns a dictionary containing the software EOL information.
